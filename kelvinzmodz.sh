f11c881357a12cb4ee23b69a6191c8d0a9e69372
kelvinadity <154925326+kelvinadity@users.noreply.github.com>
2024-04-13 16:08:10 +0700

    Add files via upload

---
+++ kelvinzmodz.sh
@@ -0,0 +1,61 @@
+#/!/bin/bash
+
+
+clear
+#hiasan
+figlet  "tau kontol ga? babi anjing"
+  echo
+echo -e "[°]================^==================[°]"
+echo -e "[°] Author    :kelvinzmodz            [°]"
+echo -e "[+] ig        :kelvinzmodz            [+]"
+echo -e "[+] Fb        :kelvinzmodz            [+]"
+echo -e "[+] team      :meta exploit           [+]"
+echo -e "[°]================^==================[°]"
+echo
+  echo -e "➘1: hack▁fb▁work▁anjing⏎"
+  echo -e "➘2: ddosattack⏎"
+  echo -e "➘3: hackkamera⏎"
+echo
+  echo -e "[★]▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁[★]"
+  echo -e "[★] masuki anggkanya kontol  [★]"
+  echo -e "[★]▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁[★]"
+echo
+read -p "▁▁▁▁▁▁▁▁➥" yut
+if [ $yut == "1" ]
+then
+clear
+    cd  $SHOME
+    pkg  update && pkg upgrade
+    pkg install python2 -y
+    pkg install nano
+    pip install futures
+    pip2 install futures
+    pip install mechanize
+    pip2 install requests bs4
+    rm -rf MBF
+    pip2 install mechanize
+    pip install requets
+    pip install src
+    pip2 install requets
+    pkg install git -y
+    git clone https://github.com/kelvinadity/MetaXploit.git
+clear
+    cd MetaXploit
+clear
+    git pull
+    python2 MBF.py
+fi
+
+if [ $syut == "2" ]
+then
+clear
+     cd $SHOME
+     pkg install git -y
+     pkg install python -y
+     pkg install python3 -y
+     git clone https://github.com/palahsu/DDoS-Ripper.git
+clear
+sleep 1
+     cd DDoS-Ripper
+     python3 DRipper.py
+fi
