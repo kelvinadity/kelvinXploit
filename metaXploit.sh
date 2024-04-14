f08bd912de00d8089a36f15e416cb36f031b4258
kelvinadity <154925326+kelvinadity@users.noreply.github.com>
2024-04-14 19:25:44 +0700

    Add files via upload

---
+++ metaXploit.sh
@@ -0,0 +1,81 @@
+#/!/bin/bash
+
+
+clear
+#hiasan
+   echo
+echo -e "
+ __   __      _       _ _   __  __            
+ \ \ / /     | |     (_) | |  \/  |           
+  \ V / _ __ | | ___  _| |_| \  / | ___ _ __  
+   > < | '_ \| |/ _ \| | __| |\/| |/ _ \ '_ \ 
+  / . \| |_) | | (_) | | |_| |  | |  __/ | | |
+ /_/ \_\ .__/|_|\___/|_|\__|_|  |_|\___|_| |_|
+       | |                                    
+       |_|                                    
+"
+echo -e "[°]================^==================[°]"
+echo -e "[°] Author    :kelvinzmodz            [°]"
+echo -e "[+] ig        :kelvinzmodz            [+]"
+echo -e "[+] Fb        :kelvinzmodz            [+]"
+echo -e "[+] team      :meta exploit           [+]"
+echo -e "[°]================^==================[°]"
+echo
+  echo -e "➘1: crackFB⏎"
+  echo -e "➘2: GOSHARE⏎"
+  echo -e "➘3: prem"
+echo
+  echo -e "[★]▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁[★]"
+  echo -e "[★] masuki anggkanya kontol  [★]"
+  echo -e "[★]▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁[★]"
+echo
+read -p "▁▁▁▁▁▁▁▁➥" yut
+if [ $yut == "1" ]
+then
+clear
+    cd $SHOME
+    pkg update  && pkg upgrade 
+    pkg install python
+    pip install mechanize
+    pip install rich
+    pip install pwinput
+    pip install requests
+    pip install bs4 
+    pip install stdiomask
+    git clone https://github.com/kelvinadity/KelvinzXploit
+clear
+    cd KelvinzXploit
+clear
+    python kelvinzV1.py
+fi
+
+if [ $yut == "2" ]
+then
+clear
+    cd $SHOME
+    pkg upgrade && pkg update
+    pkg install git
+    pkg install golang
+    cd
+    git clone https://github.com/sptty-chan/goshare
+    cd goshare
+    go build
+    ./goshare
+fi
+
+if [ $yut == "3" ]
+then
+clear
+    pkg install python
+    pip install mechanize
+    pip install rich
+    pip install pwinput
+    pip install requests
+    pip install bs4
+    pip install stdiomask
+    git clone https://github.com/kelvinadity/kelvinprem
+clear
+    cd kelvinprem
+clear
+    python kelvinzprem.py
+fi
